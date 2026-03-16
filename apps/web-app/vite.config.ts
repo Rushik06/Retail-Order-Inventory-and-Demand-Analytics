77ad4a676d85fcf65ed73edff67cdd02e777c357
Dumpala Rushik <dumpala.rushik@mindfiresolutions.com>
2026-03-16 18:33:10 +0530

    feat: implement bundle analyzer,build,start scripts

--- apps/web-app/vite.config.ts
+++ apps/web-app/vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
+import {analyzer} from "vite-bundle-analyzer"
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),analyzer()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
