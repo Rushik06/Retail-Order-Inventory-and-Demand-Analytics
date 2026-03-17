07cbf1fa4576b19f5ebaed2c95b82b8c17e0e4db
Dumpala Rushik <dumpala.rushik@mindfiresolutions.com>
2026-03-17 18:15:35 +0530

    feat: updated main.tsx file

--- apps/web-app/src/main.tsx
+++ apps/web-app/src/main.tsx
@@ -5,19 +5,25 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App";
-import GlobalErrorBoundary from "./components/GlobalErrorBoundary"; 
+import GlobalErrorBoundary from "./components/GlobalErrorBoundary";
 import "./index.css";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+
     <QueryClientProvider client={queryClient}>
+
       <GlobalErrorBoundary>
+
         <BrowserRouter>
-          <App />
+           <App />
         </BrowserRouter>
+
       </GlobalErrorBoundary>
+
     </QueryClientProvider>
+
   </React.StrictMode>
 );
