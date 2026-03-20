66aeac94d2dc9103b45ac8362066b579502744c5
Dumpala Rushik <dumpala.rushik@mindfiresolutions.com>
2026-03-21 00:43:33 +0530

    fix: fix audit issue

--- apps/web-app/eslint.config.js
+++ apps/web-app/eslint.config.js
@@ -4,14 +4,12 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import path from 'path'
+
+const __dirname = path.resolve()
 
 export default defineConfig([
-  globalIgnores([
-    '**/dist/**',
-    '**/node_modules/**',
-    '**/coverage/**',
-    '**/build/**'
-  ]),
+  globalIgnores(['**/dist/**', '**/node_modules/**', '**/coverage/**', '**/build/**']),
   {
     files: ['**/*.{ts,tsx,js,jsx}'],
     extends: [
@@ -23,6 +21,21 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        projectService: {
+          allowDefaultProject: [
+            "eslint.config.js",
+            "vite.config.ts",
+          ],
+        },
+      },
+    },
+    rules: {
+      "react-refresh/only-export-components": "warn",
+      "react-hooks/exhaustive-deps": "warn",
+      "react-hooks/set-state-in-effect": "warn",
+      "no-empty": "warn",
     },
   },
 ])
--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,9 @@
-import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import globals from "globals";
+import path from "path";
+
+const __dirname = path.resolve();
 
 export default defineConfig([
   {
@@ -13,11 +16,48 @@ export default defineConfig([
       "coverage/**",
       "tmp/**",
       "temp/**",
+      "apps/web-app/**",
+        "**/*.cjs",        
     ],
   },
+
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    languageOptions: { globals: globals.browser },
+    files: ["**/*.{ts,mts,cts}"],
+    extends: [...tseslint.configs.recommended],
+    languageOptions: {
+      globals: { ...globals.node },
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        projectService: {
+          allowDefaultProject: [
+            "*.cjs",
+            "*.js",
+            "commitlint.config.cjs",
+            "apps/*/src/config/*.cjs",
+            "apps/*/src/migrations/*.cjs",
+            "apps/*/src/seeders/*.cjs",
+          ],
+        },
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
   },
-  tseslint.configs.recommended,
+
+  {
+    files: ["**/*.cjs"],
+    rules: {
+      "@typescript-eslint/no-require-imports": "off", 
+      "@typescript-eslint/no-var-requires": "off",
+    },
+  },
+
+  {
+    files: ["**/*.{js,mjs}"],
+    languageOptions: {
+      globals: { ...globals.node },
+    },
+  },
 ]);
